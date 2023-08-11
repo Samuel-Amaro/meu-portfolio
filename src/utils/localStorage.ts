6ad17004965722edb3eee411f23755a5d7fa0586
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-11 13:10:20 -0300

    functions para obter é definir dados no localstorage

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,31 @@
+"use client";
+
+export function getLocalStorage<Type>(
+  key: string,
+  initialValue: Type | (() => Type),
+) {
+  if (typeof window !== "undefined") {
+    const data = window.localStorage.getItem(key);
+    if (!data) {
+      if (typeof initialValue === "function") {
+        return (initialValue as () => Type)();
+      } else {
+        return initialValue;
+      }
+    }
+    return JSON.parse(data) as Type;
+  } else {
+    if (typeof initialValue === "function") {
+      return (initialValue as () => Type)();
+    } else {
+      return initialValue;
+    }
+  }
+}
+
+export function setLocalStorage(key: string, value: unknown) {
+  if (typeof window !== "undefined") {
+    const data = JSON.stringify(value);
+    window.localStorage.setItem(key, data);
+  }
+}
