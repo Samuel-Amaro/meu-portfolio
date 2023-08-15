b281f05b93d9e08da4a4ef622e3e0c386051c910
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-15 10:30:37 -0300

    add estrutura html page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import SectionAbout from "@/components/SectionAbout/index";
 
 export default function Home() {
   return (
-   <h1>My Portfolio</h1>
-  )
+    <main className={styles.main}>
+      <SectionAbout />
+    </main>
+  );
 }
