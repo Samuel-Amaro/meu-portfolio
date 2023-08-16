8f589c13874f9c1a377e73cc1fafe68ccbdcdb14
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-16 10:23:21 -0300

    componente SectionExperience

---
+++ src/components/SectionExperience/index.tsx
@@ -0,0 +1,33 @@
+import styles from "./styles.module.css";
+
+//TODO: ESTILIZAR MOBILE, DESKTOP DESTA SECTION
+//TODO: CRIAR A SECTION SERVIÇOS
+
+export default function SectionExperience() {
+  return (
+    <section className="section">
+      <h2 className={`heading2 ${styles.sectionExperienceTitle}`}>
+        Experiência
+      </h2>
+      <div className={styles.sectionExperienceContainer}>
+        <article>
+          <h3 className={`heading3 ${styles.sectionExperienceCargo}`}>
+            Cargo Exercido - link empresa
+          </h3>
+          <h4 className={`heading4 ${styles.sectionExperienceEmpresa}`}>
+            Nome empresa/instituição
+          </h4>
+          <p className={styles.sectionExperienceDescription}>
+            uma descrição resumindo as tarefas, rotina diaria, ferramentas, onde
+            atuo, oque resolveu, etc... um resumo da experiencia
+          </p>
+        </article>
+        <aside>
+          <p className={styles.sectionExperiencedate}>
+            {"data inicio"} - {"data sáida"}
+          </p>
+        </aside>
+      </div>
+    </section>
+  );
+}
