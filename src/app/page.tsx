556cba98e9610696efcf6b8a1778205ee992ea31
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-16 11:12:00 -0300

    removendo section que não sera usada mais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import SectionAbout from "@/components/SectionAbout/index";
+import LineDiviser from "@/components/LineDiviser";
+import SectionEducation from "@/components/SectionEducation";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <SectionAbout />
+      <LineDiviser />
+      <SectionEducation />
     </main>
   );
 }
