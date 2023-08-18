fb41bcaa0a47ee2209113a866143af1b6d418b1c
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-18 11:42:01 -0300

    alterando nome de atributo para o react entender

--- src/components/Icons/CSSModules.tsx
+++ src/components/Icons/CSSModules.tsx
@@ -3,7 +3,7 @@ export default function IconCSSModules({ className }: { className?: string }) {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       role="img"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
