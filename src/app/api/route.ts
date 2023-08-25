a8e177bd792efb2b7f87335ab5413934ac20d74a
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-25 15:40:39 -0300

    router handler para obter dados locais, de projetos e retorna via reponse json

---
+++ src/app/api/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import path from "path";
+import { promises as fs } from "fs";
+import { Project } from "@/types/projects";
+
+//carrega dados de um arquivo json
+export async function GET(_request: Request) {
+  //Encontre o caminho absoluto do diretório json
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Leia o arquivo de dados json projects.json
+  const fileContents = await fs.readFile(
+    jsonDirectory + "/projects.json",
+    "utf8",
+  );
+  //Retorna o conteúdo do arquivo de dados no formato JSON
+  const jsonDatas = JSON.parse(fileContents) as { projects: Project[] };
+  return NextResponse.json(jsonDatas, { status: 200 });
+}
